390cf18577ec304e4a0f782e6508c17828feb74d
M33x0 <michael.mbea@gmail.com>
2022-01-28 17:36:38 +0100

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lockboxes module"""
+
+
+def canUnlockAll(boxes):
+    """Checks if all boxes can be unlocked starting from the key
+    contained in box 0"""
+    opened = [0]
+    numberOfBoxes = len(boxes)
+
+    for box in opened:
+        if box < numberOfBoxes:
+            for newKey in boxes[box]:
+                if newKey not in opened and 0 < newKey < numberOfBoxes:
+                    opened.append(newKey)
+    return True if len(opened) >= numberOfBoxes else False
