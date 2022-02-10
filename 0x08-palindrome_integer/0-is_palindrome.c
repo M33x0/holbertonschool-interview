067ceee7fdd01c860106034b02afe79a2f61505e
M33x0 <michael.mbea@gmail.com>
2022-02-10 14:31:29 +0100

    palyndrome

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "palindrome.h"
+
+/**
+ * reverse - reverses a given integer into a reversed one
+ * @n: The given integer
+ * @reversed: the reversed version of n
+ *
+ * Return: The reversed number
+ */
+unsigned long reverse(unsigned long n, unsigned long reversed)
+{
+	if (n == 0)
+		return (reversed);
+	reversed = (reversed * 10) + (n % 10);
+	return (reverse(n / 10, reversed));
+}
+
+/**
+ * is_palindrome - Checks wheter an integer is palindrome or not
+ * @n: the given integer
+ *
+ * Return: 1 if n is palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long reversed
+
+	reversed = reverse(n, 0);
+	if (n == reversed)
+		return (1);
+	return (0);
+}
