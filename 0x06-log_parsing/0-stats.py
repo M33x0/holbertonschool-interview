48ad7b9610b8c5872052f034c844da780d826b99
M33x0 <michael.mbea@gmail.com>
2022-05-03 04:13:16 +0000

    ffs

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python3
 """Log parsing"""
+from sys import stdin
 
-import sys
+if __name__ == "__main__":
 
+    statusCodes = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
+    totalSize = 0
 
-records = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-    }
-file_size = 0
-try:
-    for time, line in enumerate(sys.stdin, 1):
-        token = line.split()
-        if len(token) > 2:
-            status_code = token[len(token) - 2]
-            file_size += int(token[len(token) - 1])
-            if status_code in records:
-                records[str(status_code)] += 1
-        if time % 10 == 0:
-            print("File size: {}".format(file_size))
-            for sc in sorted(records):
-                if records[sc] > 0:
-                    print("{}: {}".format(sc, records[sc]))
-except KeyboardInterrupt:
-    pass
-finally:
-    print("File size: {}".format(file_size))
-    for sc in sorted(records):
-        if records[sc] > 0:
-            print("{}: {}".format(sc, records[sc]))
+    def parseLine(line):
+        """Parses a line of known format"""
+        global totalSize
+
+        try:
+            toks = line.rstrip().split(' ')
+            totalSize += int(toks[-1])
+
+            if int(toks[-2]) in statusCodes:
+                statusCodes[int(toks[-2])] += 1
+
+        except BaseException:
+            pass
+
+    def printStats():
+        """Prints all current stats"""
+        print("File size: {}".format(totalSize))
+        for k in sorted(statusCodes.keys()):
+            if statusCodes[k]:
+                print("{}: {}".format(k, statusCodes[k]))
+
+    lineNb = 1
+
+    try:
+        for line in stdin:
+            parseLine(line)
+            if lineNb % 10 == 0:
+                printStats()
+            lineNb += 1
+    except KeyboardInterrupt:
+        printStats()
+        raise
+    printStats()
